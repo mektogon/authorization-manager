9aa4018107df11c6e459d61abc5aa71c79213a1f
Maxim Dorofeev <Mektogon385@mail.ru>
2023-08-21 23:44:20 +0700

    AM - Создание task "createPatch" для генерации sql-скриптов liquibase.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import org.gradle.kotlin.dsl.war
+import java.util.UUID.randomUUID
 
 plugins {
     java
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ru.dorofeev"
-version = "1.0.0"
+version = "01.000.00"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -49,4 +49,85 @@ tasks.getByName<Jar>("jar") {
 
 tasks.getByName<War>("war") {
     enabled = false //Disable generation plain-file for war-package
+}
+
+tasks.register("createPatch") {
+
+    /**
+     * Добавление записи в changelog.yaml в зависимости от наличия или отсутствия файла.
+     *
+     * Если файл отсутствует - добавляется базовый текст, иначе добавляется новая запись к существующему тексту.
+     *
+     * @param file файл, в который осуществляется запись
+     * @param path относительный путь до changelog
+     */
+    fun dataWriter(file: File, path: String) {
+        val defaultDataChangelog = """
+        databaseChangeLog:
+            - include:
+                file: $path
+                relativeToChangelogFile: true
+    """.trimIndent()
+
+        val defaultDataIncludePatch = """
+    - include:
+        file: $path
+        relativeToChangelogFile: true     
+    """.trimEnd()
+
+        if (!file.exists()) {
+            file.writeText(defaultDataChangelog)
+        } else {
+            file.writeText(file.readText().plus(defaultDataIncludePatch))
+        }
+    }
+
+    if (!project.hasProperty("patchname")) {
+        throw GradleException("The name of the patch is missing!")
+    }
+
+    val versionToCatalogLiquibase: Any? = if (project.hasProperty("patchversion")) {
+        project.property("patchversion")
+    } else {
+        project.version
+    }
+
+    val basePathToFile = project.layout.projectDirectory.dir("src/main/resources/db/")
+    val pathToCurrentPatchCatalog = "$basePathToFile/$versionToCatalogLiquibase"
+    val changelogName = "changelog.yaml"
+    val resultPatchName = "${project.property("patchname")}_${randomUUID()}.sql"
+
+    var defaultDataScript = """
+        --liquibase formatted sql
+        --changeset <AUTHOR NAME>:$resultPatchName:<TASK NAME>
+    """.trimIndent()
+
+    if (project.hasProperty("type")) {
+        defaultDataScript = defaultDataScript.plus("\n--type ${project.property("type")}")
+    }
+
+    outputs.files(
+            "$basePathToFile/$changelogName", //Master-changelog
+            "$pathToCurrentPatchCatalog/$changelogName", //Local-changelog
+            "$pathToCurrentPatchCatalog/$resultPatchName" //Patch-script
+    )
+
+    doFirst {
+        outputs.files.forEach { file ->
+            if (file.name.equals(resultPatchName)) {
+                file.writeText(defaultDataScript) //Writing patch-script
+            } else {
+                if (file.absolutePath.contains(versionToCatalogLiquibase.toString())) { //Writing local-changelog
+                    dataWriter(file, "$versionToCatalogLiquibase/$resultPatchName")
+                } else { //Writing to the master-changelog
+                    if (!file.exists()) { //First writing
+                        dataWriter(file, "$versionToCatalogLiquibase/$changelogName")
+                    }
+                    if (file.exists() && !file.readText().contains(versionToCatalogLiquibase.toString())) {
+                        dataWriter(file, "$versionToCatalogLiquibase/$changelogName")
+                    }
+                }
+            }
+        }
+    }
 }
